5e25a6f12647f66825110e53abacaf17fa89ffd3
“thisnabeel” <nabeel.khan.swe@gmail.com>
2025-02-08 14:53:01 -0800

    added miro

---
+++ src/routes/video/2/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import uuid from '$lib/components/functions/uuid';
+
+	let roomId = 'taqaddum5050';
+
+	let src = `https://sfu.mirotalk.com/join?room=${roomId}&roomPassword=0&name=mirotalksfu&audio=0&video=0&screen=0&hide=0&notify=0&duration=unlimited`;
+</script>
+
+<iframe
+	allow="camera; microphone; display-capture; fullscreen; clipboard-read; clipboard-write; web-share; autoplay"
+	{src}
+	style="width: 100vw; height: 100vh; border: 0px;"
+></iframe>
