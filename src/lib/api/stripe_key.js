562e705fe16bfd8eff5e118662647b0e626351c8
“thisnabeel” <nabeel.khan.swe@gmail.com>
2025-03-06 02:39:50 -0800

    donation component

--- src/lib/api/stripe_key.js
+++ src/lib/api/stripe_key.js
@@ -1,6 +1,6 @@
 const stripe_key =
 	process.env.NODE_ENV === 'production'
-		? process.env.API_URL
+		? process.env.STRIPE_PUBLISHABLE_KEY
 		: import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY;
 
 export default stripe_key;
